df9678170b830507971a1f0e10d2d6ed85c4a318
TheCurryGuy <thecurryguyy@gmail.com>
2025-01-28 21:04:47 +0530

    Final Fixes

--- Server/src/middleware.ts
+++ Server/src/middleware.ts
@@ -1,20 +1,27 @@
 import { NextFunction, Response, Request } from "express";
-import { JWT_PASS } from "./config";
+import { JWT_PASSWORD } from "./config";
 import jwt from "jsonwebtoken";
 
 
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     const header = req.headers['token'];
-    const decoded = jwt.verify(header as string, JWT_PASS)
-    
-    if(decoded){
-        //@ts-ignore
-        req.userId = decoded.id;
-        next()
-    } else{
+    if(!JWT_PASSWORD){
         res.status(403).json({
-            message: "Invalid token"
+            message: "Server Error"
         })
+    } else {
+        const decoded = jwt.verify(header as string, JWT_PASSWORD)
+    
+        if(decoded){
+            //@ts-ignore
+            req.userId = decoded.id;
+            next()
+        } else{
+            res.status(403).json({
+                message: "Invalid token"
+            })
+        }
     }
+    
 }
